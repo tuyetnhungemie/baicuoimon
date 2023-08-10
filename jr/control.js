474023bdcecf24c7a34a719dcaddf9d999a1b077
tuyetnhungemie <140497135+tuyetnhungemie@users.noreply.github.com>
2023-08-11 01:20:44 +0700

    Add files via upload

---
+++ jr/control.js
@@ -0,0 +1,56 @@
+$('#New').owlCarousel({
+    loop:true,
+	autoplay:true,
+    margin:10,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('#sp-kid').owlCarousel({
+    loop:true,
+	autoplay:true,
+    margin:10,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('#sp-women').owlCarousel({
+    loop:true,
+	autoplay:true,
+    margin:10,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
